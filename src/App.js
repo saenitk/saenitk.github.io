b9c286dd85de005ed256e91baa785b28ab506045
Raghav Ganesh <59568704+msraghavganesh@users.noreply.github.com>
2021-09-28 13:57:15 +0530

    removed toggle button

--- src/App.js
+++ src/App.js
@@ -1,28 +1,47 @@
-import {HashRouter as Router,Switch,Route}  from 'react-router-dom';
-import './App.css';
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
 import React, { useState } from "react";
 
-import Header from './components/Header';
-import Home from './Home/home';
-import Vision from './Home/Vision';
-import Core from './Home/Core';
-import Youtube from './Home/Youtube';
-import Contact from './components/Contact';
+import Header from "./components/Header";
+import Home from "./Home/home";
+import Vision from "./Home/Vision";
+import Core from "./Home/Core";
+import Youtube from "./Home/Youtube";
+import Contact from "./components/Contact";
 
-import About from './AboutUs/About';
+import About from "./AboutUs/About";
 
-import App2 from './Blogs/Blogsmain';
+import App2 from "./Blogs/Blogsmain";
 
-import Club from './Members/Club';
-import Alumni from './Members/alumni';
+import Club from "./Members/Club";
+import Alumni from "./Members/alumni";
 
-import Projects1 from './Projects/projects1';
-import Projects2 from './Projects/projects2';
-import Magazine from './Projects/magazine';
+import Projects1 from "./Projects/projects1";
+import Projects2 from "./Projects/projects2";
+import Magazine from "./Projects/magazine";
 
-import EventMain from './Event/Event';
-import {Event1_21, Event2_21, Event3_21, Event4_21, Event5_21, Event6_21, Event7_21, Event8_21, Event9_21, Event10_21} from './Event/Events/eventpage_21';
-import {Event1_20, Event2_20, Event3_20, Event4_20, Event5_20, Event6_20,Event7_20} from './Event/Events/eventpage_20';
+import EventMain from "./Event/Event";
+import {
+  Event1_21,
+  Event2_21,
+  Event3_21,
+  Event4_21,
+  Event5_21,
+  Event6_21,
+  Event7_21,
+  Event8_21,
+  Event9_21,
+  Event10_21,
+} from "./Event/Events/eventpage_21";
+import {
+  Event1_20,
+  Event2_20,
+  Event3_20,
+  Event4_20,
+  Event5_20,
+  Event6_20,
+  Event7_20,
+} from "./Event/Events/eventpage_20";
 //import { Carousel } from 'bootstrap';
 
 function App() {
@@ -31,62 +50,97 @@ function App() {
   return (
     <Router className="router">
       <Header />
-       <Switch>
-       <div className="app" data-theme={darkMode ? "dark" : "light"}>
-<input type="checkbox" onClick={toggleDarkMode} id="toggle-mode-cb" />
-
-<div id="mode-wrapper">
-  
-  <label id="toggle-mode" for="toggle-mode-cb">
-    <a class="sr-only">Toggle</a>
-    <a class="toggle-border">
-      <a class="toggle-indicator"></a>
-    </a>
-  </label>
-</div>
-
-        <Route exact path={process.env.PUBLIC_URL+'/'}>
-          <Home />
-          <Vision />
-          <Core />
-          <Youtube />
-        </Route>
-        <Route exact path="/sae-nitk">
-          <Home />
-          <Vision />
-          <Core />
-          <Youtube />
-        </Route>
-        <Route exact path="/about-us"><About /></Route>
-        <Route exact path="/blog"><App2 /></Route>
-        <Route exact path="/project-expo-2019-2020"><Projects1 /></Route>
-        <Route exact path="/project-expo-2020-2021">
+      <Switch>
+        <div className="app" data-theme={darkMode ? "dark" : "light"}>
+          
+          <Route exact path={process.env.PUBLIC_URL + "/"}>
+            <Home />
+            <Vision />
+            <Core />
+            <Youtube />
+          </Route>
+          <Route exact path="/sae-nitk">
+            <Home />
+            <Vision />
+            <Core />
+            <Youtube />
+          </Route>
+          <Route exact path="/about-us">
+            <About />
+          </Route>
+          <Route exact path="/blog">
+            <App2 />
+          </Route>
+          <Route exact path="/project-expo-2019-2020">
+            <Projects1 />
+          </Route>
+          <Route exact path="/project-expo-2020-2021">
             <Projects2 />
             <Magazine />
-        </Route>
-        <Route exact path="/club"><Club /></Route>
-        <Route exact path="/alumni"><Alumni/></Route>
-        <Route exact path="/events"><EventMain /></Route>
+          </Route>
+          <Route exact path="/club">
+            <Club />
+          </Route>
+          <Route exact path="/alumni">
+            <Alumni />
+          </Route>
+          <Route exact path="/events">
+            <EventMain />
+          </Route>
 
-        <Route exact path="/events/magazine-release"><Event9_21/></Route>
-        <Route exact path="/events/website-release"><Event10_21/></Route>
+          <Route exact path="/events/magazine-release">
+            <Event9_21 />
+          </Route>
+          <Route exact path="/events/website-release">
+            <Event10_21 />
+          </Route>
 
-        <Route exact path="/events/webinar-by-r-s-pant"><Event1_21/></Route>
-        <Route exact path="/events/matlab-kep"><Event2_21/></Route>
-        <Route exact path="/events/simulink-kep"><Event3_21/></Route>
-        <Route exact path="/events/solidworks-kep"><Event4_21/></Route>
-        <Route exact path="/events/adobe"><Event5_21/></Route>
-        <Route exact path="/events/cosmol"><Event6_21/></Route>
-        <Route exact path="/events/internship-webinar"><Event7_21/></Route>
-        <Route exact path="/events/interactive-session-on-gate"><Event8_21/></Route>
+          <Route exact path="/events/webinar-by-r-s-pant">
+            <Event1_21 />
+          </Route>
+          <Route exact path="/events/matlab-kep">
+            <Event2_21 />
+          </Route>
+          <Route exact path="/events/simulink-kep">
+            <Event3_21 />
+          </Route>
+          <Route exact path="/events/solidworks-kep">
+            <Event4_21 />
+          </Route>
+          <Route exact path="/events/adobe">
+            <Event5_21 />
+          </Route>
+          <Route exact path="/events/cosmol">
+            <Event6_21 />
+          </Route>
+          <Route exact path="/events/internship-webinar">
+            <Event7_21 />
+          </Route>
+          <Route exact path="/events/interactive-session-on-gate">
+            <Event8_21 />
+          </Route>
 
-        <Route exact path="/events/autocad-workshop"><Event1_20/></Route>
-        <Route exact path="/events/the-mind-fiz"><Event2_20/></Route>
-        <Route exact path="/events/kasting"><Event3_20/></Route>
-        <Route exact path="/events/matlab-and-simulink-workshop"><Event4_20/></Route>
-        <Route exact path="/events/otto-quiz"><Event5_20/></Route>
-        <Route exact path="/events/keynote-talk-series"><Event6_20/></Route>
-        <Route exact path="/events/career-building-kep"><Event7_20/></Route>
+          <Route exact path="/events/autocad-workshop">
+            <Event1_20 />
+          </Route>
+          <Route exact path="/events/the-mind-fiz">
+            <Event2_20 />
+          </Route>
+          <Route exact path="/events/kasting">
+            <Event3_20 />
+          </Route>
+          <Route exact path="/events/matlab-and-simulink-workshop">
+            <Event4_20 />
+          </Route>
+          <Route exact path="/events/otto-quiz">
+            <Event5_20 />
+          </Route>
+          <Route exact path="/events/keynote-talk-series">
+            <Event6_20 />
+          </Route>
+          <Route exact path="/events/career-building-kep">
+            <Event7_20 />
+          </Route>
         </div>
       </Switch>
       <Contact />
